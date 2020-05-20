ba9d96a8e71ae6ed9880f52ff7b8aacd3f1cd0af
Larry_A <larryagbana@gmail.com>
2020-05-20 09:44:49 -0400

    convert Post model to use sequelize classes

--- models/Post.js
+++ models/Post.js
@@ -1,18 +1,32 @@
-module.exports = (sequelize, DataTypes) =>
-  sequelize.define('Post', {
-    location: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: { len: [1] }
+const { Model } = require('sequelize')
+
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {}
+
+  Post.init(
+    {
+      location: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: { len: [1] }
+      },
+      travelExperience: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: { len: [1] }
+      },
+      imageURL: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: { len: [1] }
+      }
     },
-    travelExperience: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: { len: [1] }
-    },
-    imageURL: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: { len: [1] }
+    {
+      sequelize
     }
-  })
+  )
+
+  Post.associate = models => Post.belongsTo(models.User)
+
+  return Post
+}
