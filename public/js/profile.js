0ea6023f088bd94b6ac27d2e5c8f53990bb6d7bf
Larry_A <larryagbana@gmail.com>
2020-05-25 08:41:39 -0400

    script - handling profile page interaction

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,66 +1,54 @@
-// const bioBTN = document.querySelector('#user-bio')
-// const postBTN = document.querySelector('#user-post')
-// const recommendationBTN = document.querySelector('#user-recommendations')
-// const settingsBTN = document.querySelector('#user-bio')
-// const bio = document.querySelector('#bio')
-// const post = document.querySelector('#post')
-// const recommendations = document.querySelector('#recommendations')
-// const settings = document.querySelector('#settings')
-
+const bio = document.querySelector('#bio')
+const posts = document.querySelector('#post')
+const recommendations = document.querySelector('#recommendations')
+const settings = document.querySelector('#settings')
+const bioBTN = document.querySelector('#user-bio')
+const postBTN = document.querySelector('#user-post')
+const recommendationBTN = document.querySelector('#user-recommendations')
+const settingsBTN = document.querySelector('#user-settings ')
 const btnsContainer = document.getElementById('btns-DIV')
 const btns = btnsContainer.getElementsByClassName('btns')
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener('click', function () {
-    const current = document.getElementsByClassName('active')
-    console.log(current[0])
-    current[0].className = current[0].className.replace('active', '')
+    const currentBTN = document.getElementsByClassName('active')
+    currentBTN[0].className = currentBTN[0].className.replace('active', '')
     if (this.className !== 'active') {
       this.className += ' active'
     }
   })
 }
 
-// bioBTN.addEventListener('click', function () {
-// //   bio.style.display = 'block'
+bioBTN.addEventListener('click', function () {
+  bio.style.display = 'block'
 
-//   // Hide other page divs
-// //   post.style.display = 'none'
-// //   recommendations.style.display = 'none'
-// //   settings.style.display = 'none'
-//   /*
-//     Make other buttons inactive
-//     Reference for using Regex: https://www.w3schools.com/howto/howto_js_remove_class.asp
-//   */
-// //   postBTN.className = postBTN.className.replace(/\bmystyle\b/g, '')
-// //   postBTN.className = postBTN.className.replace(/\bmystyle\b/g, '')
-// //   recommendationBTN.className = recommendationBTN.className.replace(/\bmystyle\b/g, '')
-// //   settings.className = settings.className.replace(/\bmystyle\b/g, '')
-// })
+  // Hide other page divs
+  posts.style.display = 'none'
+  recommendations.style.display = 'none'
+  settings.style.display = 'none'
+})
 
-// postBTN.addEventListener('click', function () {
-// //   post.style.display = 'block'
+postBTN.addEventListener('click', function () {
+  posts.style.display = 'block'
 
-//   // Hide other page divs
-// //   document.querySelector('#bio').style.display = 'none'
-// //   document.querySelector('#recommendations').style.display = 'none'
-// //   document.querySelector('#settings').style.display = 'none'
-// })
+  // Hide other page divs
+  bio.style.display = 'none'
+  recommendations.style.display = 'none'
+  settings.style.display = 'none'
+})
 
-// recommendationBTN.addEventListener('click', function () {
-// //   recommendations.style.display = 'block'
+recommendationBTN.addEventListener('click', function () {
+  recommendations.style.display = 'block'
+  // Hide other page divs
+  bio.style.display = 'none'
+  posts.style.display = 'none'
+  settings.style.display = 'none'
+})
 
-//   // Hide other page divs
-// //   document.querySelector('#bio').style.display = 'none'
-// //   document.querySelector('#post').style.display = 'none'
-// //   document.querySelector('#settings').style.display = 'none'
-// })
-
-// settingsBTN.addEventListener('click', function () {
-// //   settings.style.display = 'block'
-
-//   // Hide other page divs
-// //   document.querySelector('#post').style.display = 'none'
-// //   document.querySelector('#recommendations').style.display = 'none'
-// //   document.querySelector('#bio').style.display = 'none'
-// })
+settingsBTN.addEventListener('click', function () {
+  settings.style.display = 'block'
+  // Hide other page divs
+  posts.style.display = 'none'
+  recommendations.style.display = 'none'
+  bio.style.display = 'none'
+})
